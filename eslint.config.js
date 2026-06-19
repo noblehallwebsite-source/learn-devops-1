3567120784da934588add65081306b2df9bab8d2
Noble <noblehallwebsite@gmail.com>
2026-06-19 10:22:46 +0100

    fix: add flat config file for eslint v10

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+// eslint.config.js
+module.exports = [
+  {
+    rules: {
+      "semi": ["error", "always"],     // Enforce semicolons at the end of statements
+      "no-unused-vars": "warn"         // Warn if variables are defined but never used
+    }
+  }
+];
